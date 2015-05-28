74706eebf86aab0648a5da0a753d5ee01cc57bc1
Jorge Vasquez <jorge.vasquez@unir.net>
2015-05-28 12:15:25 -0400

    ejemplos con operadores

--- 04-operadores.rb
+++ 04-operadores.rb
@@ -8,7 +8,8 @@
 # -: Definición de valor negativo
 # !: Operador lógico NOT
 # ~: Bitwise: complemento
-
+puts 'Operadores unarios'
+puts '------------------'
 puts "+1 => #{+1}"
 puts "-1 => #{-1}"
 puts "!1 => #{!1}"
@@ -16,6 +17,8 @@ puts "~1100 => #{~1100}"
 
 # Aritméticos
 # +, -, *, /, %
+puts 'Operadores Aritméticos'
+puts '----------------------'
 puts "1 + 1 = #{1+1}"
 
 # En ruby, los tipos básicos son objetos, asi que cada operador corresponde a un metodo
@@ -28,8 +31,25 @@ puts "10./(4.0) = #{10./(4.0)}"
 a = 0
 b = 1
 c = 2
+puts 'Operadores de Comparación'
+puts '-------------------------'
 puts "a = #{a}, b = #{b}"
 puts "a <= b : #{a <= b}"
 puts "b <=> a: #{b <=> a}"
-puts "b <=> b: #{b <= b}"
-puts "b <=> c: #{b <= c}"
+puts "b <=> b: #{b <=> b}"
+puts "b <=> c: #{b <=> c}"
+
+# igualdad
+# ==, ===, !=, =~, !~, .eql?, .equal?
+# 
+puts 'Operadores de igualdad'
+puts '----------------------'
+puts "1 == 1: #{1 == 1}"
+# === se usa en las comprobaciones dentro de un rango. En este caso en un case/switch
+puts "(5..10) === 7: #{(5..10) === 7}"
+puts "(5..10) === 4: #{(5..10) === 4}"
+
+puts "'abc' =~ /b/ : #{'abc' =~ /b/}"
+puts "'abc' =~ /z/ : #{'abc' =~ /z/}"
+puts "'123456789' !~ /['a'-'Z']/ : #{'123456789' !~ /['a'-'Z']/}"
+puts "'a' !~ /['a'-'Z']/ : #{'a' !~ /['a'-'Z']/}"
